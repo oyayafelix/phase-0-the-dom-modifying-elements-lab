bb7405f80f60dc56ea8e040dbc9cb749b2778cf4
oyayafelix <oyayafelix@gmail.com>
2022-06-13 16:23:22 +0300

    solution

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+main.remove();
+
+const newHeader = document.createElement("h1"); newHeader.id = 'victory';
+newHeader.innerHTML = 'Felix is the champion';
+document.body.append(newHeader);
